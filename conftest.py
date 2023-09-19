236a58ca2236014641c8894c192569135e910bd4
Vadzimmm <vadzimbelashtsentau@gmail.com>
2023-09-19 20:36:06 +0300

    Добавлено закрытие браузера.

--- conftest.py
+++ conftest.py
@@ -6,6 +6,10 @@ from selene import browser
 def open_browser():
     browser.open("https://www.google.com/")
 
+    yield
+
+    browser.quit()
+
 @pytest.fixture(autouse=True)
 def setting_browser():
     browser.driver.set_window_size(1920, 1080)
